4ae15bab922c1a1880f12b79d2ff0c9744d8cb41
trung2711 <tug57907@temple.edu>
2020-06-05 19:22:56 -0400

    Updates

---
+++ precache-manifest.06a6cf3a8d35bb401cacfb5d14a730bf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b21820cb5ad42520be86b3d25143c40",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "02b42f495a248c3ca5a4",
+    "url": "/my-portfolio/static/css/2.3665d427.chunk.css"
+  },
+  {
+    "revision": "d3847a6714f893572c81",
+    "url": "/my-portfolio/static/css/main.2316eca6.chunk.css"
+  },
+  {
+    "revision": "02b42f495a248c3ca5a4",
+    "url": "/my-portfolio/static/js/2.32b1648c.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/my-portfolio/static/js/2.32b1648c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3847a6714f893572c81",
+    "url": "/my-portfolio/static/js/main.dde53c2e.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  },
+  {
+    "revision": "80c846984dd14d4d52288fc415b6bff0",
+    "url": "/my-portfolio/static/media/Burger-Icon.80c84698.svg"
+  },
+  {
+    "revision": "9ff8323fe30f64b60092e369f01490e7",
+    "url": "/my-portfolio/static/media/Chatbot.9ff8323f.gif"
+  },
+  {
+    "revision": "4fb0035de865fb18a17cb4922ff73ba0",
+    "url": "/my-portfolio/static/media/Dark-Header.4fb0035d.svg"
+  },
+  {
+    "revision": "5fc0435a57219fcd78353cc3909afee0",
+    "url": "/my-portfolio/static/media/GitHub-Light.5fc0435a.svg"
+  },
+  {
+    "revision": "72a4a5dfd613f99ed388431001e0498f",
+    "url": "/my-portfolio/static/media/Header-Photo.72a4a5df.jpg"
+  },
+  {
+    "revision": "e20c6d5b1f0fba72effe5be4a029dd1e",
+    "url": "/my-portfolio/static/media/Image-Translate.e20c6d5b.gif"
+  },
+  {
+    "revision": "7104a785c57008cdf410c7fa8bba30d8",
+    "url": "/my-portfolio/static/media/ImageSelection.7104a785.png"
+  },
+  {
+    "revision": "1c542329d672ac7919d0f063f2eea7db",
+    "url": "/my-portfolio/static/media/LinguaBox.1c542329.svg"
+  },
+  {
+    "revision": "6d6dd3f3ad214d91e488e7aebc1ae19f",
+    "url": "/my-portfolio/static/media/LinkedIn-Light.6d6dd3f3.svg"
+  },
+  {
+    "revision": "8b54b6cccf7c381c923ee9f214ebcba8",
+    "url": "/my-portfolio/static/media/NextButton.8b54b6cc.svg"
+  },
+  {
+    "revision": "aa4b8d74ee11dc3091feac24926cd3fd",
+    "url": "/my-portfolio/static/media/PrevButton.aa4b8d74.svg"
+  },
+  {
+    "revision": "6c7e85a71827b12a50b1c07cb9f96871",
+    "url": "/my-portfolio/static/media/Profile.6c7e85a7.png"
+  },
+  {
+    "revision": "2fdf59136cf9c47fb3f46c9cfa8ef1a6",
+    "url": "/my-portfolio/static/media/SlidePuzzle.2fdf5913.gif"
+  },
+  {
+    "revision": "8aabdfd2ae15510d7efaa60a33183db7",
+    "url": "/my-portfolio/static/media/SlidePuzzle.8aabdfd2.svg"
+  },
+  {
+    "revision": "d92023b03089a829c5cecd3642e02a93",
+    "url": "/my-portfolio/static/media/Statistics.d92023b0.png"
+  },
+  {
+    "revision": "a6ff87fe2d765fbdd3b16cc0463b4a01",
+    "url": "/my-portfolio/static/media/TournamentPortal.a6ff87fe.svg"
+  },
+  {
+    "revision": "b74e6d9c5b1576f5329eebda85723467",
+    "url": "/my-portfolio/static/media/White-Bg.b74e6d9c.svg"
+  }
+]);
